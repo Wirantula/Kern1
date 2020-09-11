c849e4b04b1314447b0761b8d8de72f190dcbc2b
peer <masterfruitieapple@hotmail.com>
2020-09-11 16:09:13 +0200

    added stalactieten
    werkt nog met monobehavior

---
+++ KernModule1/Assets/Scripts/Fsm_Peer/DemonLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonLogic
+{
+    /* 
+     * wat doet een demon?
+     schiet vuurballen
+     slaat met armen
+     spuwd vuur
+     
+     * wat weet een demon
+     als hij iets raakt (collision)
+     waar de speler is
+
+    */
+}
---
+++ KernModule1/Assets/Scripts/Fsm_Peer/StateMachine/FSMOwner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMOwner : MonoBehaviour
+{
+    EnvoirmentIdle State1;
+    StateMachine voorbeeldMachine;
+    void Start()
+    {
+        State1 = new EnvoirmentIdle(voorbeeldMachine); //create states 
+        voorbeeldMachine = new StateMachine(State1); //create statemachine
+        voorbeeldMachine.OnStart(State1);            //parse states to machine
+        
+    }
+    void FixedUpdate() //50 ticks a sec
+    {
+        if(Input.GetKey(KeyCode.A) == true)
+        {
+            voorbeeldMachine.OnUpdate();                 //update for state machine
+        }
+       
+    }
+}
